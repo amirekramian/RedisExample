0c8a2956082e63372cf1f3d9a0e31cc57c1d1936
amir <amirekramian232@gmail.com>
2022-06-12 13:45:02 -0700

    add basic configs

---
+++ RedisTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisTest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IConnectionMultiplexer client;
+
+        public HomeController(IConnectionMultiplexer client)
+        {
+            this.client= client;
+        }
+
+
+        [HttpGet("")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            await client.GetDatabase(0).SetAddAsync("folder","setting");
+            return Content("ok");
+        }
+    }
+}
---
+++ RedisTest/Program.cs
@@ -0,0 +1,50 @@
+
+
+
+using StackExchange.Redis;
+using StackExchange.Redis.Extensions.Core.Configuration;
+using StackExchange.Redis.Extensions.Newtonsoft;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var redisConfiguration =builder.Configuration.GetSection("Redis").Get<RedisConfiguration>();
+builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = redisConfiguration.Hosts[0].Host + ":" + redisConfiguration.Hosts[0].Port; });
+builder.Services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfiguration);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    ConnectionMultiplexer.Connect("localhost:6379")
+    );
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
